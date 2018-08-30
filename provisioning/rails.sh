76f6988e6a296ca0a6628b9ed9ca0304390af4b9
ocsarpo <ocsarpo22@gmail.com>
2018-08-30 10:28:21 +0900

    디버깅 젬 설치과정

--- provisioning/rails.sh
+++ provisioning/rails.sh
@@ -6,10 +6,12 @@ curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
-sudo apt-get update
+sudo apt-get update  -y 
+sudo apt-get upgrade  -y 
+sudo apt-get install -y build-essential
 sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs yarn -y
 
-echo "루비 설치"
+echo "루비 설치 시작"
 if [ ! -d ~/.rbenv ]; then
     git clone https://github.com/rbenv/rbenv.git ~/.rbenv
     echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
@@ -26,18 +28,25 @@ export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"
 eval "$(rbenv init -)"
 
 if [ ! -e .rbenv/versions/2.5.1 ]; then
-echo "2.5.1 설치"
+  echo "ruby 2.5.1 설치"
   rbenv install 2.5.1    
   rbenv global 2.5.1
   ruby -v
 fi
+echo "루비 설치 끝"
 
 if [ ! -e .rbenv/shims/bundle ]; then
+  echo "번들러 설치"
   gem install bundler
   rbenv rehash
 fi
 
-echo "Rails 설치"
+echo "Rails 설치 시작"
 gem install rails -v 5.2.0
 rbenv rehash
 rails -v
+echo "Rails 설치 끝"
+
+echo "디버깅 젬 설치"
+gem install ruby-debug-ide 
+gem install debase
